3c1eeb14d8745ee8888cd2d004bf6b0c18491b1c
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-04-25 15:07:07 -0300

    04_basic

---
+++ 01_basic/05_input.py
@@ -0,0 +1,14 @@
+#pedir informacion al usuario
+
+
+nombre=input("hola como te llamas?: ")
+nombre=int(input("que edad tienes?: "))
+
+print(f"{nombre}")
+
+print("Multiples valores")
+
+country, city=input("en que pai y ciudad vives? : ").split()
+
+print(f"viven en {country} ciudad de {city} ")
+
