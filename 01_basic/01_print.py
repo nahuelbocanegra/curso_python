e59f87a56b4b61a45a1020c4f24d25f435f747de
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-04-25 15:03:34 -0300

    01_basic

---
+++ 01_basic/01_print.py
@@ -0,0 +1,10 @@
+#mostrar informacion en consola
+print("hola mundo")
+print('esto funcion')
+print('python','es', "genial")
+print('python','es', "muy",'bueno', sep="-") #sep => separador
+print('esto se imprime',end='!')
+print('en una linea') #por defecto el print hace una salto de linea en cada print, poniendo el end, se remplaza el salto por un ! en este caso
+print(23)
+
+
