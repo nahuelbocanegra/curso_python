f157dab789d0a3ba0d0614d8b12ddc7a0d1d2c1a
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-04-25 15:06:15 -0300

    02_basic

---
+++ 01_basic/02_types.py
@@ -0,0 +1,33 @@
+#tipos de datos 
+
+#enteros
+print(type(10)) #inprime en consola el tipo de dato del valor
+print(1)
+print(12)
+print(10+2)
+
+print("float")
+
+print(type(3.2))
+print(type(1.2))
+print(type(.2))
+
+print("numeros complejos")
+print(type(3j))
+
+print("str")
+print("hola")
+print("123")
+
+print("booleanos")
+print(False)
+print(True)
+
+
+print("ausencia de valor")
+print(None)
+
+
+
+
+
