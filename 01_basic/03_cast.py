54d481544c634d901ab039b44ed55c71f9d8b1b0
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-04-25 15:06:42 -0300

    03_basic

---
+++ 01_basic/03_cast.py
@@ -0,0 +1,17 @@
+
+#transformacion de datos casting
+
+print("conversion de datos")
+print(2+int("100"))
+print("100" + str(2))
+print(float(100))
+
+print(bool(3))
+print(bool(-1))
+print(bool(0))
+
+print(bool(""))#false
+print(bool(" "))#true
+print(bool("False"))#true
+
+
---
+++ 01_basic/04_variables.py
@@ -0,0 +1,39 @@
+name="juan"
+print(name)
+
+#asignar una variable
+age=32
+print(age)
+
+age=40
+print(age)
+
+#tipado dinamico, el tipo de dato de la variable puede cambiar en momento de ejecucion 
+
+nombre="nahuel"
+
+name=32
+
+#tipado fuerte no realiza conversiones de tipo automatico
+
+print(10+"2")
+print(f"nombre: {nombre} edad: {age}")
+
+#no se recomienda
+nombre, edad, ciudad = "nahuel",23,"cordoba"
+
+#convencion de nombre de la variables
+
+ciudad_usuario="cordoba" #snake_case
+MiNombre="Nahuel" #pascal_case
+
+
+#python no tiene constantes
+
+CONSTANTE="cordoba" #se puede simular con mayusculas por convencion 
+
+#es una anotacion ,en python en momento de ejecucion no lo cumple
+user: str = "nahuel"
+user=23
+
+
