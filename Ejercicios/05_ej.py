cdf103a32687227e5cbba396b5d563c400cea484
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-04-28 16:14:41 -0300

    05:ej:m

--- Ejercicios/05_ej.py
+++ Ejercicios/05_ej.py
@@ -1,4 +1,14 @@
 """Escribir una función que reciba una frase y 
 devuelva un diccionario con las palabras que contiene y su longitud.
 """
+text="Escribir una función que reciba una frase y devuelva un diccionario con las palabras que contiene y su longitud"
+def contar_longitud(text):
+    lista=text.split()
+    dicc={}
 
+    for palabra in lista:
+        dicc[palabra]=len(palabra)
+
+    return dicc
+
+print(contar_longitud(text))
