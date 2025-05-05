66639bf774a08c3ea1664a208acd3d11c455d9de
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-05-05 10:32:11 -0300

    regex_modificadores

--- 04_regex/02_modificadores.py
+++ 04_regex/02_modificadores.py
@@ -11,8 +11,25 @@ text = "Todo el mundo dice que la iA nos va a quitar el trabajo. Pero solo hace 
 "la ia no estan mala, viva la Ia"
 pattern = "IA"
 
-found_ia=re.findall(pattern,text,re.IGNORECASE)
+found_ia=re.findall(pattern,text,re.IGNORECASE) #IGNORECASE => no distingue entre may y minuscula
 
 if found_ia:
-    print(found_ia)
+    print(found_ia) #devuelve toda las condiciones IA
+
+
+#remplazar el texto
+
+
+text = "Todo el mundo dice que la iA nos va a quitar el trabajo. Pero solo hace falta ver cómo la puede cagar con las Regex para ir con cuidado." \
+"la ia no estan mala, viva la Ia"
+
+#sub() reemplazar todas las coincidencias de un patron de un texto
+
+text = " hola,mundo ! Hola de nuevo"
+pattern = "Hola"
+remplacement="Adios"
+
+new_text= re.sub(pattern,remplacement,text)
+
+print(new_text)
 
