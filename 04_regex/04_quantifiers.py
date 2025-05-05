975b67e98fe1963c65b8e4baa99f08fdf5e339b8
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-05-05 12:43:23 -0300

    queantifiers

---
+++ 04_regex/04_quantifiers.py
@@ -0,0 +1,36 @@
+import re 
+
+# 03 quantifiers
+#los cuantificadores se utilizan para especificar cuantas ocurrencias de 
+# un caracteres se deben encontrar en una cadena
+
+# * : puede aparecer 0 o mas veces
+text ="aaaba"
+pattern="a*"
+matches=re.findall(pattern,text)
+print(matches)
+
+
+#cuantas palabras tienen de 0 a mas "a" y despues una b
+
+# + una a mas veces
+text="aa bb cc aa dd aa"
+pattern="a+"
+matches=re.findall(pattern,text)
+print(matches)
+
+# ? cero a una vez
+
+text="aaabacb"
+pattern="a?b"
+matches=re.findall(pattern,text)
+print(matches)
+
+#{n} extamente n veces 
+
+text="aaaaaa    aa  aaa  asaa"
+pattern=r"a{3}" #devuelve las 3 primeras
+
+matches=re.findall(pattern,text)
+
+print(matches)
