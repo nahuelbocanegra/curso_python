d3a065521f3827f4e382242ec1be801ce115f2d8
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-05-13 13:59:58 -0300

    desafio5

---
+++ Ejercicios/ejercicio_ISPC/05_desafio.py
@@ -0,0 +1,13 @@
+# 5- CuandolaTierra completa una órbita alrededor del Sol, no han transcurrido
+#  exactamente 365 rotaciones sobre sí misma, sino un poco más. Más precisamente, la
+#  diferencia es de más o menos uncuarto de día.
+#  Para evitar que las estaciones se desfasen con el calendario, el calendario juliano
+#  introdujo la regla de introducir un día adicional en los años divisibles por 4 (llamados
+#  bisiestos), para tomar en consideración los cuatro cuartos de día acumulados.
+#  Sin embargo, bajo esta regla sigue habiendo un desfase, que es de aproximadamente
+#  3/400 de día.
+#  Para corregir este desfase, en el año 1582 el papa Gregorio XIII introdujo un nuevo
+#  calendario, en el que el último año de cada siglo dejaba de ser bisiesto, a no ser que
+#  fuera divisible por 400.
+#  Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era
+#  el calendario vigente en ese año
