88b2f85766e6daff38743a1f42ebfea74e3e2e85
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-05-13 13:58:39 -0300

    desafio4

--- Ejercicios/ejercicio_ISPC/04_desafio.py
+++ Ejercicios/ejercicio_ISPC/04_desafio.py
@@ -4,3 +4,23 @@
 #  entregue como resultado el tiempo total de viaje en formato horas:minutos.
 #  El programa deja de pedir tiempos de viaje cuando se ingresa un 0.
 
+def tiempo_de_viaje():
+    tiempo_total=0
+    
+    while True:
+        
+        tramo=input("ingresar el tiempo por tramo en minutos, ingrese 0 para finalizar : ")
+       
+        if int(tramo) == 0:
+            break
+        if tramo.isnumeric():
+            tiempo_total+=int(tramo)
+
+    
+    horas=tiempo_total//60
+    minutos= tiempo_total-horas*60
+
+    
+    return f"tiempo total del viaje {horas}hs:{minutos}m"
+
+print(tiempo_de_viaje())
