5440df5b8f86e8060149b19f17ebee2138268f92
Nahuel Bocanegra <nahuboca7@gmail.com>
2025-05-15 20:52:37 -0300

    desafio9

---
+++ Ejercicios/ejercicio_ISPC/09_desafio.py
@@ -0,0 +1,7 @@
+#  9-Anagrama. Escribe una función que reciba dos palabras y retorne
+#  verdadero o falso según sean o no anagramas.
+#  ● UnAnagramaconsisteenformarunapalabrareordenandoTODASlas letras de
+#  otra palabra inicial.
+#  ● NOhacefaltacomprobarqueambaspalabrasexisten.
+#  ● Dospalabrasexactamenteiguales no son anagrama
+
