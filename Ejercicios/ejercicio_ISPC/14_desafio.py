f94ead9e3e43645f823c558b05d40319001810a4
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-05-20 20:42:42 -0300

    desafio14

---
+++ Ejercicios/ejercicio_ISPC/14_desafio.py
@@ -0,0 +1,19 @@
+#  14- Signozodiacal. El signo zodiacal de una persona está determinado por su día de
+#  El diccionario de signos asocia a cada signo el período del año que le corresponde. Cada
+#  período es una tupla con la fecha de inicio y la fecha de término, y cada fecha es una
+# tupla
+
+signos_zodiaco = {
+    "Aries":       ((3, 21), (4, 19)),
+    "Tauro":       ((4, 20), (5, 20)),
+    "Géminis":     ((5, 21), (6, 20)),
+    "Cáncer":      ((6, 21), (7, 22)),
+    "Leo":         ((7, 23), (8, 22)),
+    "Virgo":       ((8, 23), (9, 22)),
+    "Libra":       ((9, 23), (10, 22)),
+    "Escorpio":    ((10, 23), (11, 21)),
+    "Sagitario":   ((11, 22), (12, 21)),
+    "Capricornio": ((12, 22), (1, 19)),
+    "Acuario":     ((1, 20), (2, 18)),
+    "Piscis":      ((2, 19), (3, 20))
+}
