eab4843f61dcee1be8cecc7479bfe24eaf5f5633
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-05-20 12:47:47 -0300

    Anagrama

--- Ejercicios/ejercicio_ISPC/09_desafio.py
+++ Ejercicios/ejercicio_ISPC/09_desafio.py
@@ -1,7 +1,19 @@
 #  9-Anagrama. Escribe una función que reciba dos palabras y retorne
 #  verdadero o falso según sean o no anagramas.
-#  ● UnAnagramaconsisteenformarunapalabrareordenandoTODASlas letras de
+#  ● Un Anagrama consiste en formar una palabra reordenando TODAS las letras de
 #  otra palabra inicial.
-#  ● NOhacefaltacomprobarqueambaspalabrasexisten.
-#  ● Dospalabrasexactamenteiguales no son anagrama
+#  ● NO hace falta comprobar que ambas palabras existen.
+#  ● Dos palabras exactamente iguales no son anagrama
 
+def anagrama (str1,str2):
+
+    if str1 == str2:
+        return False
+    
+    str1="".join(sorted(str1))
+    str2="".join(sorted(str1))
+
+    if str1 == str2:
+        return True
+    
+    return False
