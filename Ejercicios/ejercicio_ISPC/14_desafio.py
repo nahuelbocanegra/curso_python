0f6a7322ae2b0c093d1779a61966b175a2d0ce3f
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-05-21 11:17:04 -0300

    desafio_14

--- Ejercicios/ejercicio_ISPC/14_desafio.py
+++ Ejercicios/ejercicio_ISPC/14_desafio.py
@@ -3,17 +3,27 @@
 #  período es una tupla con la fecha de inicio y la fecha de término, y cada fecha es una
 # tupla
 
-signos_zodiaco = {
-    "Aries":       ((3, 21), (4, 19)),
-    "Tauro":       ((4, 20), (5, 20)),
-    "Géminis":     ((5, 21), (6, 20)),
-    "Cáncer":      ((6, 21), (7, 22)),
-    "Leo":         ((7, 23), (8, 22)),
-    "Virgo":       ((8, 23), (9, 22)),
-    "Libra":       ((9, 23), (10, 22)),
-    "Escorpio":    ((10, 23), (11, 21)),
-    "Sagitario":   ((11, 22), (12, 21)),
-    "Capricornio": ((12, 22), (1, 19)),
-    "Acuario":     ((1, 20), (2, 18)),
-    "Piscis":      ((2, 19), (3, 20))
-}
+
+def zodiaco(dia,mes):
+    
+    signos_zodiaco = {
+        "Aries":       ((3, 21), (4, 19)),
+        "Tauro":       ((4, 20), (5, 20)),
+        "Géminis":     ((5, 21), (6, 20)),
+        "Cáncer":      ((6, 21), (7, 22)),
+        "Leo":         ((7, 23), (8, 22)),
+        "Virgo":       ((8, 23), (9, 22)),
+        "Libra":       ((9, 23), (10, 22)),
+        "Escorpio":    ((10, 23), (11, 21)),
+        "Sagitario":   ((11, 22), (12, 21)),
+        "Capricornio": ((12, 22), (1, 19)),
+        "Acuario":     ((1, 20), (2, 18)),
+        "Piscis":      ((2, 19), (3, 20))
+    }
+
+    for signo,((mes_ini,dia_ini),(mes_fin,dia_fin)) in signos_zodiaco.items():
+        if (mes_ini == mes and dia >= dia_ini) or (mes_fin == mes and dia <= mes_fin ):
+            return signo
+          
+
+zodiaco(30,11)
