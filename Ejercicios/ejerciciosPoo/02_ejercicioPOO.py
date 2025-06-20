1880703932c7f4e9360d76f09eb1036ecff7d695
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-06-20 16:43:04 -0300

    fun area perimetro

---
+++ Ejercicios/ejerciciosPoo/02_ejercicioPOO.py
@@ -0,0 +1,18 @@
+# 2. Área y Perímetro. Crea una clase Rectángulo que permita calcular su área y su
+#  perímetro. Además, crea en una aplicación de consola que permita al usuario crear
+#  un objeto Perímetro y realizar los cálculos. .
+
+
+class Rectangulo:
+
+    def __init__(self,base,altura):
+        self.base=base
+        self.altura=altura
+    
+    def perimetro(self):
+        pass
+
+    def area(self):
+        pass
+        
+        
