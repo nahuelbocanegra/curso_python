cebb2c5c227bc554401e9527ab208ffcd00927e6
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-06-23 21:04:33 -0300

    ejercicio4Poo

---
+++ Ejercicios/ejerciciosPoo/04_ejercicioPOO.py
@@ -0,0 +1,7 @@
+#  4. Cuenta Bancaria. Crea una clase CuentaBancaria con los siguientes atributos
+#  obligatorios: número de cuenta, nombre y apellido. El saldo debe comenzar con
+#  100.000. Agrega e implementa métodos para: 1- depositar (debe aceptar un valor
+#  entero y sumarlo al saldo), 2- retirar (debe aceptar un valor entero y restarlo al saldo
+#  sólo si hay dinero suficiente para retirar en la cuenta) y 3- ver saldo. Además, escribe
+#  una aplicación de consola que permita al usuario depositar, retirar o ver saldo hasta
+#  que decida detenerse. Al finalizar deberá mostrar los datos de la cuenta y el saldo.
