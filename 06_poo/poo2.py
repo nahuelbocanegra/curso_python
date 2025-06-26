4ef90285a1f45416bbf9d0ebdb1aed207af46189
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-06-26 17:24:39 -0300

    archivo poo2

--- 06_poo/poo2.py
+++ 06_poo/poo2.py
@@ -1,2 +1,34 @@
 
+#modularizacion => dividir un programa grande en módulos más pequeños y manejables
+#encapsulacion =>  permite controlar el acceso a los atributos y métodos de una clase
+#metodos de acceso => 
+
+
+class Auto:  # modelo de caracteristicas comunes de un grupo de objetos
+    
+    #__init__ => constructor =>  estado inicial de las clase
+
+    #propiedades
+    largoChasis=250
+    anchiChasis=120
+    ruedas=4
+    enMarcha=False
+
+    def __init__(self):
+        pass
+
+    #metodos
+
+    def arrancar(self):
+        pass
+    def frenar(self):
+        pass
+
+    
+    
+
+
+
+auto=Auto() # instancia => ejemplar pertenecen a una clase
+
 
