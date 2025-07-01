9617f61fff9c9e749d39a7e4f2d10008540961b4
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-06-30 22:14:59 -0300

    06_ejercicioPOO

---
+++ Ejercicios/ejerciciosPoo/06_ejercicioPOO.py
@@ -0,0 +1,8 @@
+# 6. Control de Volúmen. Crea una clase ControlVolumen que permita establecer el
+#  volumen de un parlante (1 mínimo volumen, 10 máximo volumen). El constructor
+#  establecerá el volumen en un nivel medio. Agrega métodos para: 1- ajustar el
+#  volumen permitiendo que el mismo sume o reste sin salirse de los límites y 2
+# mostrar el nivel de volúmen actual. Además, escribe una aplicación de consola que
+#  permita al usuario manipular y consultar el volumen hasta que decida salir. Al
+#  finalizar deberá mostrar el nivel actual de volumen.
+
