205ac6c26eeaf8a5dc6d29b85274280cbe347cc3
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-07-07 15:02:28 -0300

    flask

---
+++ 07_flask/project_1/hello.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__) #instancia de clase 
+
+#__name__ es el nombre del modulo o app 
+
+@app.route("/") # route() indica que url activa la funcion
+def hola_mundo():
+    return "<p> hola mundo </p>"
+
+@app.route("/<name>") #ruta con parametro
+def hello(name):
+    return f"Hello,{escape(name)}!" #La función escape se usa para proteger tu aplicación web contra ataques XSS (Cross-Site Scripting)
+    
+#reglas de variables
+#  puedes usar un conversor para especificar el tipo de argumento
+        #<converter:variable_name>
+
+@app.route("/post/<int:post_id>")
+def user_profile(username):
+    return f"User {escape(username)}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
