dd74110ad7ffa951ab97a1a538fae6c4881e2f7e
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-07-13 23:44:58 -0300

    mostrando detalles

--- 07_flask/project_2/app.py
+++ 07_flask/project_2/app.py
@@ -43,5 +43,15 @@ if __name__ == "__main__":
 def inicio():
     cursos=Cursos.query.all()
     total_cursos=Cursos.query.count()
+
     
     return render_template("index.html",total=total_cursos,datos=cursos)
+
+@app.route("/curso/<int:id>")
+def ver_curso(id):
+    curso=Cursos.query.get(id) #buscar el registro en la base de datos
+    return render_template("cursos_detalle.html",dato=curso)
+
+@app.route("/insertar_curso",["GET","POST"])
+def insertar_curso():
+    pass 
