d1969337df9a7b8a09d0ff64cd4ef899fde59279
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-07-15 23:15:56 -0300

    conexion base de datos ,ysql

--- 07_flask/project_3/app.py
+++ 07_flask/project_3/app.py
@@ -1,6 +1,16 @@
 from flask import Flask ,render_template 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped,mapped_column
+from variables_entorno import DB_NAME,USER_NAME,SERVER_NAME,USER_PASSWORD,SECRET_KEY
 
 app=Flask(__name__)
+
+#conectando base de datos con alchemy
+FULL_URL_DB=f"mysql+pymysql://{USER_NAME}:{USER_PASSWORD}@{SERVER_NAME}/{DB_NAME}"
+
+app.config['SQLALCHEMY_DATABASE_URI']=FULL_URL_DB
+
+
 
 @app.route("/")
 def inicio():
