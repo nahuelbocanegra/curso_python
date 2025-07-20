f0f025cf31aa7f2389f62e42c60464f691da09b0
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-07-19 22:43:49 -0300

    models

---
+++ 07_flask/project_4/models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped,mapped_column
+from sqlalchemy import String
+
+class Contacto():
+    id:Mapped[int]=mapped_column(primary_key=True)
+    nombre:Mapped[str]=mapped_column(String(100))
+    telefono:Mapped[str]=mapped_column(String(100))
+    email:Mapped[str]=mapped_column(String(100))
+    direccion:Mapped[str]=mapped_column(String(100))
+    comentario:Mapped[str]=mapped_column(String(100))
+    
+    def __str__(self):
+        return f"""
+            nombre:{self.nombre}
+            telefono:{self.direccion}
+            email:{self.email}
+        """
