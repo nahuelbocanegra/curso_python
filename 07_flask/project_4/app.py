f0b59dc1b52112c0db5b06939d17482ed2c204f3
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-07-19 23:01:09 -0300

    CONECT BD

--- 07_flask/project_4/app.py
+++ 07_flask/project_4/app.py
@@ -1,6 +1,22 @@
 from flask import Flask,render_template
+from db import db
+from flask_migrate import Migrate
+from variables_entorno import NAME_BASE,NAME_USER,SERVER_NAME,SECRET_KEY,USER_PASSSWORD
 
 app=Flask(__name__)
+
+FULL_URL_DB=f"mysql+pymysql://{NAME_USER}:{USER_PASSSWORD}@{SERVER_NAME}/{NAME_BASE}"
+
+
+app.config["SQLALCHEMY_DATABASE_URI"]=FULL_URL_DB
+
+
+db.init_app(app)
+migrate=Migrate(db,app)
+
+
+
+
 
 @app.route("/")
 def inicio():
--- 07_flask/project_4/models.py
+++ 07_flask/project_4/models.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
+
 from sqlalchemy.orm import Mapped,mapped_column
 from sqlalchemy import String
 
