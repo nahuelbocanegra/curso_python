b149a2b0a2ac6971254be9516a1992b3ea7486d1
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-07-23 19:53:01 -0300

    detalles_contacto

--- 07_flask/project_4/forms.py
+++ 07_flask/project_4/forms.py
@@ -4,9 +4,9 @@ from wtforms.validators import DataRequired
 
 
 class NuevoContacto(FlaskForm):
-    nombre=StringField("Nombre",validators=[DataRequired])
-    telefono=StringField("Telefono",validators=[DataRequired])
-    email=StringField("Email",validators=[DataRequired])
+    nombre=StringField("Nombre",validators=[DataRequired()])
+    telefono=StringField("Telefono",validators=[DataRequired()])
+    email=StringField("Email",validators=[DataRequired()])
     direccion=StringField("Direccion",)
     comentario=TextAreaField("comentario")
     enviar=SubmitField("Enviar")
