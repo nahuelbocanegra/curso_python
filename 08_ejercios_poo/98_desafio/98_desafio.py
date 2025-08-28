06ecf5fd1b7fd0ba4dcde0b8d1b4cd0d9e0a049b
Nahuel Bocanegra <nahuelbocanegra@users.noreply.github.com>
2025-08-28 15:52:53 -0300

    desafio98

---
+++ 08_ejercios_poo/98_desafio/98_desafio.py
@@ -0,0 +1,31 @@
+#   Crea un programa que simule el mecanismo de participación:
+#   - Mediante la terminal, el programa te preguntará si quieres añadir y borrar
+#     participantes, mostrarlos, lanzar el sorteo o salir.
+#   - Si seleccionas añadir un participante, podrás escribir su nombre y pulsar enter.
+#   - Si seleccionas añadir un participante, y este ya existe, avisarás de ello.
+#     (Y no lo duplicarás)
+#   - Si seleccionas mostrar los participantes, se listarán todos.
+#   - Si seleccionas eliminar un participante, podrás escribir su nombre y pulsar enter.
+#     (Avisando de si lo has eliminado o el nombre no existe)
+#   - Si seleccionas realizar el sorteo, elegirás una persona al azar
+#     y se eliminará del listado.
+#   - Si seleccionas salir, el programa finalizará.
+ 
+
+
+class Participacion:
+
+    def añadir_participante():
+        pass
+    def eliminar_participante():
+        pass
+    def mostrar_participante():
+        pass
+    def lanzar_sorteo():
+        pass
+
+
+
+
+def inicio():
+    print("añadir y borrar participantes, mostrarlos, lanzar el sorteo o salir")
