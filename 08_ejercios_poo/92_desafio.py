a288a0f090598e457ffd2929f84539616743fe99
nahuel bocanegra <nahuboca7@gmail.com>
2025-10-08 02:55:14 +0200

    cambios

---
+++ 08_ejercios_poo/92_desafio.py
@@ -0,0 +1,5 @@
+# crea una funcion que encuentre todo los triples
+# pitagoricos(terna) menores o iguales al numero dado
+
+def triples_pitagoricos(num1,num2,num3):
+    pass
---
+++ 08_ejercios_poo/95_desafio.py
@@ -0,0 +1,23 @@
+# crea una funcion que calcule el punto de encuentro  de dos objetos en movimiento
+# en dos dimensiones
+# cada objeto esta compuesto por una cordenada xy y una velocidad de desplazamiento (vector de desplazamiento)
+# por unidad de tiempo tambien en formato 
+#    la funcion recibira las coordenadas de inicio de ambos objetos y sus velocidades
+#    la funcion calculara y mostrar el punto en el que se encuentran el tiempo tardan en lograrlo
+#    la funcion debe tener en cuenta que los objetos no pueden llegar a encontrarse  
+
+
+objeto1={
+    "cordenadas":[4,5],
+    "velocidad":10
+}
+objeto2={
+    "cordenadas":[],
+    "velocidad":10
+}
+
+def punto_encuentro(objeto1,objeto2):
+    pass
+
+
+punto_encuentro()
