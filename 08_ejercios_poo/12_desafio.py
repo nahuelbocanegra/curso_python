ed252716e16fe22310c2245fd3bf34c4f3272854
nahuel bocanegra <nahuboca7@gmail.com>
2025-10-13 20:21:58 +0200

    12desafio

---
+++ 08_ejercios_poo/12_desafio.py
@@ -0,0 +1,25 @@
+#   Crea una función que reciba dos cadenas como parámetro (str1, str2)
+#   e imprima otras dos cadenas como salida (out1, out2).
+#   - out1 contendrá todos los caracteres presentes en la str1 pero NO
+#     estén presentes en str2.
+#   - out2 contendrá todos los caracteres presentes en la str2 pero NO
+#      estén presentes en str1.
+
+
+def generar_cadenas(str1,str2):
+
+    out1=""
+  
+    for i in str1:
+        if i not in str2:
+            out1+=i
+
+       
+        str2=str2.replace(i,"")  
+        
+    print(out1)
+    print(str2)
+ 
+
+generar_cadenas("lalos","hola")
+   
